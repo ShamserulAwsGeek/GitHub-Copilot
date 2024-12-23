ac5d6c47075be7fcc8ae82dd70012c9327350e06
Md Shamserul Haque <44093151+ShamserulAwsGeek@users.noreply.github.com>
2024-12-23 17:32:06 +0530

    Update skills.js

--- skills.js
+++ skills.js
@@ -16,3 +16,28 @@ function divide(a, b) {
     }
     return a / b;
 }
+function modulus(a, b) {
+    return a % b;
+}
+
+function power(a, b) {
+    return Math.pow(a, b);
+}
+
+function squareRoot(a) {
+    return Math.sqrt(a);
+}
+
+function absolute(a) {
+    return Math.abs(a);
+}
+
+function factorial(a) {
+    if (a < 0) return undefined;
+    if (a === 0 || a === 1) return 1;
+    let result = 1;
+    for (let i = a; i > 1; i--) {
+        result *= i;
+    }
+    return result;
+}
